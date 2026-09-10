dcaccc0ed2e006d43bdce3e86e040d20d6f1ee5d
rehan3988966-debug <rehan3988966@gmail.com>
2026-09-10 18:09:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello! Mera pehla Docker App chal raha hai! 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})}
